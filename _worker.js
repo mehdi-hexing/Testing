ba6b51191149735408efed9adaf6d43ff74c5a6b
mehdi-hexing <127022233+mehdi-hexing@users.noreply.github.com>
2025-06-04 03:42:38 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -310,10 +310,10 @@ async function 整理(内容) { //
 
 async function 双重哈希(文本) { //
   const 编码器 = new TextEncoder(); //
-  const 第一次哈希 = await crypto.subtle.digest('MD5', 编码器.encode(文本)); //
+  const 第一次哈希 = await crypto.subtle.digest('SHA-256', 编码器.encode(文本)); //
   const 第一次哈希数组 = Array.from(new Uint8Array(第一次哈希)); //
   const 第一次十六进制 = 第一次哈希数组.map(字节 => 字节.toString(16).padStart(2, '0')).join(''); //
-  const 第二次哈希 = await crypto.subtle.digest('MD5', 编码器.encode(第一次十六进制.slice(7, 27))); //
+  const 第二次哈希 = await crypto.subtle.digest('SHA-256', 编码器.encode(第一次十六进制.slice(7, 27))); //
   const 第二次哈希数组 = Array.from(new Uint8Array(第二次哈希)); //
   const 第二次十六进制 = 第二次哈希数组.map(字节 => 字节.toString(16).padStart(2, '0')).join(''); //
   return 第二次十六进制.toLowerCase(); //
